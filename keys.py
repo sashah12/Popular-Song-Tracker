f6359dab11e595310d13da69162139a161b39c1c
sashah12 <46096116+sashah12@users.noreply.github.com>
2019-06-12 13:45:54 -0700

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,6 @@
+#tokens taken from twitter
+
+CONSUMER_KEY = 'your consumer_key here'
+CONSUMER_SECRET = 'your consumer_secret here'
+ACCESS_KEY = 'your access_key here'
+ACCESS_SECRET = 'your access_secret here'
